c0372759e09f8c80a1c9387f1aadb84ffb811d08
shahzeb7860 <69763218+shahzeb7860@users.noreply.github.com>
2022-12-13 19:19:51 +0530

    Add files via upload

---
+++ API.py
@@ -0,0 +1,48 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+import requests
+import os
+from datetime import datetime
+import pymongo
+
+
+user_api = "126e98a4dfdacf3fb707ab0a15398072"
+location = input("Enter the city name: ")
+
+complete_api_link = "https://api.openweathermap.org/data/2.5/weather?q="+location+"&appid="+user_api
+api_link = requests.get(complete_api_link)
+api_data = api_link.json()
+
+#create variables to store and display data
+temp_city = ((api_data['main']['temp']) - 273.15)
+temp_city_feelslike = ((api_data['main']['feels_like']) - 273.15)
+date_time = datetime.now().strftime("%d %b %Y | %I:%M:%S %p")
+
+print ("-------------------------------------------------------------")
+print ("Weather Stats for - {}  || {}".format(location.upper(), date_time))
+print ("-------------------------------------------------------------")
+
+print ("Current temperature is: {:.2f} deg C".format(temp_city))
+print ("Current temperature feels like is: {:.2f} deg C".format(temp_city_feelslike))
+
+#  -------------   MONGODB   ---------
+
+client = pymongo.MongoClient('mongodb://127.0.0.1:27017')
+mydb = client['Weather']
+information =mydb.Weatherinformation
+record ={
+        'City_name' : location,
+        'temp_city' : temp_city,
+        'temp_city_feelslike' : temp_city_feelslike,
+        'date_time' : date_time
+        }
+
+information.insert_one(record)
+
+
+
